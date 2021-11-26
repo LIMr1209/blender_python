dbf506c7b94d3c0ffffd4382ea5faeafb54a8507
LIMr1209 <aaa1058169464@126.com>
2021-11-26 10:26:59 +0800

    bug

--- test.py
+++ test.py
@@ -1,41 +1,36 @@
 from scipy.spatial.transform import Rotation as R
+import math
 
-origin_r = R.from_euler('zyx', [-34.6, 59.0, -63.0], degrees=True)
-m = origin_r.as_matrix()
-
-# Euler((radians(-34.6), radians(59.0), radians(-63.0)), 'zyx').to_matrix()
-
-print(m)
 '''
+-34.6, 59.0, -63.0
     three.js
-        0.2338223930133095,
-        -0.9543938894679361,
-        0.18563241169524922,
-        0.4589022849502757,
-        -0.05998983159917603,
-        -0.8864591998350059,
-        0.8571673007021124,
-        0.2924611493018649,
-        0.4239465706384317,
-    blender  
+        [[ 0.42394657  0.29246115  0.8571673 ]
+          [-0.8864592  -0.05998983  0.45890228]
+          [ 0.18563241 -0.95439389  0.23382239]]
+    blender
     Matrix(((0.23382212221622467, 0.4589025378227234, 0.8571672439575195),
         (-0.18563242256641388, 0.8864590525627136, -0.4239468574523926),
         (-0.9543939828872681, -0.05998987704515457, 0.29246091842651367)))
 '''
+# three.js 转 blender
+origin_r = R.from_euler('zyx', [3.44, 46.41, 9.17], degrees=True)
+m = origin_r.as_matrix()
 
 
-def convert_matrix(m):
-    return [[m[2][2], m[1][2], m[0][2]],
-            [-m[2][0], -m[1][0], -m[0][0]],
-            [m[2][1], m[1][1], m[0][1]],
-            ]
+def matrix_convert_three_to_blend(m):
+    return [
+        [m[2][2], m[1][2], m[0][2]],
+        [-m[2][0], -m[1][0], -m[0][0]],
+        [m[2][1], m[1][1], m[0][1]],
+    ]
 
 
-new_m = convert_matrix(m)
-print(new_m)
+new_m = matrix_convert_three_to_blend(m)
 r = R.from_matrix(new_m)
 print(r.as_euler('xyz', degrees=True))
 
+
+# blender 脚本
 # from mathutils import Euler, Matrix
 # from math import radians
 #
@@ -45,3 +40,20 @@ print(r.as_euler('xyz', degrees=True))
 # matrix = Matrix(b_m)
 # euler = matrix.to_euler('XYZ')
 
+# blender 转 three.js
+origin_r = R.from_euler('xyz', [math.degrees(1.6130332946777344), math.degrees(-0.203330859541893),
+                                math.degrees(0.8023278713226318)], degrees=True)
+m = origin_r.as_matrix()
+
+
+def matrix_convert_blend_to_three(m):
+    return [
+        [-m[1][2], m[2][2], m[0][2]],
+        [-m[1][1], m[2][1], m[0][1]],
+        [-m[1][0], m[2][0], m[0][0]],
+    ]
+
+
+new_m = matrix_convert_blend_to_three(m)
+r = R.from_matrix(new_m)
+print(r.as_euler('zyx', degrees=True))
